afff1b7604b33a6f29055ce4af9cc2738a6a142a
Glitch (coral-speckle-swing) <none>
2024-06-02 08:03:38 +0000

    👖👻 Checkpoint

--- scripts/cmds/onchat.js
+++ scripts/cmds/onchat.js
@@ -14,7 +14,7 @@ module.exports = {
   }, 
 
   onChat: async function ({ api, message, event }) {
-    const prefix = ":";
+    const prefix = global.config.prefix;
     if (event.body && event.body.toLowerCase() == "prefix") {
       const a = await message.reply(`✨ 𝖠𝗐𝖺𝗅𝖺𝗇 𝗋𝖺𝖿𝖿𝖺 𝖺𝖽𝖺𝗅𝖺𝗁: [ ${prefix} ]`);
       setTimeout(() => { api.editMessage(`𝖦𝗎𝗇𝖺𝗄𝖺𝗇 ${prrfix}𝗆𝖾𝗇𝗎 𝗎𝗇𝗍𝗎𝗄 𝗆𝖾𝗅𝗂𝗁𝖺𝗍 𝖽𝖺𝖿𝗍𝖺𝗋 𝗉𝖾𝗋𝗂𝗇𝗍𝖺𝗁!`, a.messageID); }, 16000);
