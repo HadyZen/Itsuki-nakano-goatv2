c32f7f73092cb8b0294abe6e266a75c6faf1e515
Hady Zen <149669803+HadyZen@users.noreply.github.com>
2024-06-30 17:10:01 +0700

    Update and rename restart.js to restar.js

--- scripts/cmds/restar.js
+++ scripts/cmds/restar.js
@@ -2,18 +2,18 @@ const fs = require("fs-extra");
 
 module.exports = {
  config: {
- name: "restart",
+ name: "restar",
  version: "1.1",
  author: "NTKhang",
  countDown: 6,
  role: 2,
- description: "𝖬𝖾𝗆𝗎𝗅𝖺𝗂 𝗎𝗅𝖺𝗇𝗀 𝗋𝖺𝖿𝖿𝖺",
+ description: "𝖬𝖾𝗆𝗎𝗅𝖺𝗂 𝗎𝗅𝖺𝗇𝗀 𝗂𝗍𝗌𝗎𝗄𝗂",
  category: "ADMIN",
  guide: { id: "{pn}" }
 },
 
 	onLoad: function ({ api }) {
-		const pathFile = `${__dirname}/tmp/restart.txt`;
+		const pathFile = `${__dirname}/tmp/restar.txt`;
 		if (fs.existsSync(pathFile)) {
 			const [tid, time] = fs.readFileSync(pathFile, "utf-8").split(" ");
 			api.sendMessage(`✨ 𝖲𝖾𝗅𝖾𝗌𝖺𝗂 𝗆𝖾𝗆𝗎𝗅𝖺𝗂 𝗎𝗅𝖺𝗇𝗀 (${(Date.now() - time) / 1000}s)`, tid);
@@ -22,9 +22,9 @@ module.exports = {
 	},
 
 	onStart: async function ({ message, event }) {
-		const pathFile = `${__dirname}/tmp/restart.txt`;
+		const pathFile = `${__dirname}/tmp/restar.txt`;
 		fs.writeFileSync(pathFile, `${event.threadID} ${Date.now()}`);
-		await message.reply("✨ 𝖬𝖾𝗆𝗎𝗅𝖺𝗂 𝗎𝗅𝖺𝗇𝗀 𝗋𝖺𝖿𝖿𝖺...");
+		await message.reply("✨ 𝖬𝖾𝗆𝗎𝗅𝖺𝗂 𝗎𝗅𝖺𝗇𝗀 𝗂𝗍𝗌𝗎𝗄𝗂...");
 		process.exit(2);
 	}
 };
