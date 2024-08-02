fbc4009dc9f74a1e7ccbc329abf020bf772ea6c0
Hady Zen <149669803+HadyZen@users.noreply.github.com>
2024-08-02 20:17:45 +0700

    Hady zen

--- scripts/cmds/setn.js
+++ scripts/cmds/setn.js
@@ -1,7 +1,8 @@
+const font = require("fontstyles");
 module.exports = {
   config: {
     name: "setn",
-    version: "1.0",
+    version: "1.1",
     author: "Hady Zen",
     countDown: 20,
     role: 0,
@@ -11,28 +12,38 @@ module.exports = {
   },
 
   onStart: async function ({ message, args, event, usersData }) { 
-    const nama = args.join(' ');
-  if (!nama) { return message.reply('𝖲𝗂𝗅𝖺𝗁𝗄𝖺𝗇 𝗍𝗎𝗅𝗂𝗌𝖺𝗄𝖺𝗇 𝗇𝖺𝗆𝖺𝗆𝗎 𝗍𝗎𝖺𝗇!');
+const { gender, name } = await usersData.get(event.senderID);
+const names = name.split(" ")[0];
+const nama = font.bold(names);
+let kelamin = "";
+if (gender == 1) { 
+ kelamin += font.bold("-chan");
+} else if (gender == 2) { 
+ kelamin += font.bold("-kun");
+} 
+    const miku = args.join(' ');
+  if (!miku) { return message.reply(nama + kelamin + ', 𝗄𝖺𝗆𝗎 𝖻𝖾𝗅𝗎𝗆 𝗆𝖾𝗆𝖺𝗌𝗎𝗄𝗄𝖺𝗇 𝗇𝖺𝗆𝖺 𝗒𝖺𝗇𝗀 𝗄𝖺𝗆𝗎 𝗂𝗇𝗀𝗂𝗇𝗄𝖺𝗇!');
   } 
   if (nama.length > 14) {
-    return message.reply('𝖭𝖺𝗆𝖺 𝗄𝖺𝗆𝗎 𝗅𝖾𝖻𝗂𝗁 𝖽𝖺𝗋𝗂 𝟣𝟦 𝗁𝗎𝗋𝗎𝖿!')
+    return message.reply(nama + kelamin + ', 𝗄𝖺𝗆𝗎 𝗆𝖾𝗆𝖺𝗌𝗎𝗄𝗄𝖺𝗇 𝗇𝖺𝗆𝖺 𝗅𝖾𝖻𝗂𝗁 𝖽𝖺𝗋𝗂 𝟭𝟰 𝗄𝖺𝗋𝖺𝗄𝗍𝖾𝗋!')
   }
    const data = await usersData.get(event.senderID);
  const hadi = data.money;
- if (hadi < 1) { return message.reply('𝖪𝖺𝗆𝗎 𝗆𝖾𝗆𝖻𝗎𝗍𝗎𝗁𝗄𝖺𝗇 𝟣 𝗒𝖾𝗇 𝗎𝗇𝗍𝗎𝗄 𝗆𝖾𝗇𝗀𝗀𝖺𝗇𝗍𝗂 𝗇𝖺𝗆𝖺');
+ if (hadi < 3) { return message.reply(nama + kelamin + ', 𝗄𝖺𝗆𝗎 𝗆𝖾𝗆𝖻𝗎𝗍𝗎𝗁𝗄𝖺𝗇 𝟮 𝗒𝖾𝗇 𝗎𝗇𝗍𝗎𝗄 𝗆𝖾𝗇𝗀𝗀𝖺𝗇𝗍𝗂 𝗇𝖺𝗆𝖺.');
 }
 if (nama == "reset") { 
   await usersData.set(event.senderID, { 
      money: hadi - 2,
      data: { ...data.data, nama: '' }
 });
-   return message.reply('𝖭𝖺𝗆𝖺 𝗄𝖺𝗆𝗎 𝖻𝖾𝗋𝗁𝖺𝗌𝗂𝗅 𝖽𝗂 𝗌𝖾𝗍𝖾𝗅 𝗎𝗅𝖺𝗇𝗀');
+   return message.reply(nama + kelamin + ', 𝗇𝖺𝗆𝖺 𝗄𝖺𝗆𝗎 𝖻𝖾𝗋𝗁𝖺𝗌𝗂𝗅 𝖽𝗂 𝗌𝖾𝗍𝖾𝗅 𝗎𝗅𝖺𝗇𝗀!');
 } else {
   await usersData.set(event.senderID, {
      money: hadi - 2,
-    data: { ...data.data, nama: nama }
+    data: { ...data.data, nama: miku }
   });
-   message.reply(`𝖭𝖺𝗆𝖺 𝗄𝖺𝗆𝗎 𝖻𝖾𝗋𝗁𝖺𝗌𝗂𝗅 𝖽𝗂 𝗎𝖻𝖺𝗁 𝗆𝖾𝗇𝗃𝖺𝖽𝗂 ${nama}`);
+   const itsuki = font.bold(`${nama}${kelamin}, 𝗇𝖺𝗆𝖺 𝗄𝖺𝗆𝗎 𝖻𝖾𝗋𝗁𝖺𝗌𝗂𝗅 𝖽𝗂 𝗎𝖻𝖺𝗁 𝗆𝖾𝗇𝗃𝖺𝖽𝗂 ${miku}.`);
+   message.reply(itsuki);
 }
  }, 
 };
