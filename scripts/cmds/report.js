b6486e092694dd514b68e20654bc47eb11a4b6dd
Hady Zen <149669803+HadyZen@users.noreply.github.com>
2024-08-02 14:35:01 +0700

    Hady zen

--- scripts/cmds/report.js
+++ scripts/cmds/report.js
@@ -1,4 +1,5 @@
 const { getStreamsFromAttachment } = global.utils;
+const font = require("fontstyles");
 module.exports = {
   config: {
     name: "report",
@@ -8,47 +9,56 @@ module.exports = {
     role: 0,
     description: "𝗅𝖺𝗉𝗈𝗋𝗄𝖺𝗇 𝖻𝗎𝗀 𝗄𝖾 𝖺𝖽𝗆𝗂𝗇 𝗂𝗍𝗌𝗎𝗄𝗂", 
     category: "SISTEM",
-    guide: { id: "{pn} <𝗉𝖾𝗌𝖺𝗇>" }
+    guide: { id: "{pn} <𝗉𝖾𝗌𝖺𝗇/𝗌𝖾𝗇𝖽>" }
   },
 
   onStart: async function ({ message, api, event, usersData, args, role }) { 
+const { gender, name } = await usersData.get(event.senderID);
+const names = name.split(" ")[0];
+const nama = font.bold(names);
+let kelamin = "";
+if (gender == 1) { 
+ kelamin += font.bold("-chan");
+} else if (gender == 2) { 
+ kelamin += font.bold("-kun");
+} 
    switch(args[0]) {
      case 'send':
-       if (role < 2) { return message.reply('𝖪𝖺𝗆𝗎 𝗍𝗂𝖽𝖺𝗄 𝗉𝗎𝗇𝗒𝖺 𝖼𝗎𝗄𝗎𝗉 𝗂𝗓𝗂𝗇 𝗎𝗇𝗍𝗎𝗄 𝗆𝖾𝗇𝗀𝗀𝗎𝗇𝖺𝗄𝖺𝗇 𝗉𝖾𝗋𝗂𝗇𝗍𝖺𝗁 𝗂𝗇𝗂!');
+       if (role < 2) { return message.reply(nama + kelamin + ', 𝖼𝗎𝗆𝖺 𝗁𝖺𝖽𝗂-𝗌𝖺𝗇 𝗒𝖺𝗇𝗀 𝖻𝗂𝗌𝖺 𝗆𝖾𝗇𝗀𝗀𝗎𝗇𝖺𝗄𝖺𝗇 𝗂𝗇𝗂!');
 }
   const id = args[1];
   const hadi = args.slice(2).join(' ');
-       if (!hadi) { return message.reply('𝖬𝖺𝗌𝗎𝗄𝖺𝗇 𝗉𝖾𝗌𝖺𝗇 𝗆𝗎 𝗍𝗎𝖺𝗇! (◍•ᴗ•◍)');
+       if (!hadi) { return message.reply(nama + kelamin + ', 𝗄𝖺𝗆𝗎 𝖻𝖾𝗅𝗎𝗆 𝗆𝖾𝗆𝖺𝗌𝗎𝗄𝗄𝖺𝗇 𝖻𝖺𝗅𝖺𝗌𝖺𝗇 𝗅𝖺𝗉𝗈𝗋𝖺𝗇𝗇𝗒𝖺!');
 }
  const hady = await usersData.getName(event.senderID);
- const itsuki = `♡ 𝗕𝗮𝗹𝗮𝘀𝗮𝗻 𝗹𝗮𝗽𝗼𝗿𝗮𝗻
+ const itsuki = font.thin(`♡ 𝗕𝗮𝗹𝗮𝘀𝗮𝗻 𝗹𝗮𝗽𝗼𝗿𝗮𝗻
  
  𝖣𝖺𝗋𝗂: ${hady}
- 𝖯𝖾𝗌𝖺𝗇: ${hadi}`;
+ 𝖯𝖾𝗌𝖺𝗇: ${hadi}`);
 const hanib = { body: itsuki, attachment: await getStreamsFromAttachment(event.messageReply?.attachments || []) 
 		};
    api.sendMessage(hanib, id);
-   message.reply('𝖯𝖾𝗌𝖺𝗇 𝖻𝖾𝗋𝗁𝖺𝗌𝗂𝗅 𝖽𝗂𝗄𝗂𝗋𝗂𝗆 𝗌𝖾𝗇𝗉𝖺𝗂! (◍•ᴗ•◍)');
+   message.reply(nama + kelamin + ', 𝖻𝖺𝗅𝖺𝗌𝖺𝗇 𝗄𝖺𝗆𝗎 𝖻𝖾𝗋𝗁𝖺𝗌𝗂𝗅 𝖽𝗂𝗄𝗂𝗋𝗂𝗆!');
      break;
      default: 
   const pesan = args.join(' ');
-       if (!pesan) { return message.reply('𝖪𝖺𝗆𝗎 𝖻𝖾𝗅𝗎𝗆 𝗆𝖾𝗆𝖺𝗌𝗎𝗄𝗄𝖺𝗇 𝗉𝖾𝗌𝖺𝗇𝗇𝗒𝖺! (◍•ᴗ•◍)');
+       if (!pesan) { return message.reply(nama + kelamin + ', 𝗄𝖺𝗆𝗎 𝖻𝖾𝗅𝗎𝗆 𝗆𝖾𝗆𝖺𝗌𝗎𝗄𝗄𝖺𝗇 𝗅𝖺𝗉𝗈𝗋𝖺𝗇 𝗒𝖺𝗇𝗀 𝗂𝗇𝗀𝗂𝗇 𝖽𝗂𝗄𝗂𝗋𝗂𝗆!');
 }
   const ID_GC = "7756208531066471";
   const grup = await api.getThreadInfo(event.threadID); 
   const gcNama = grup.name;
-  const nama = await usersData.getName(event.senderID);
-  const laporan = `♡ 𝗟𝗮𝗽𝗼𝗿𝗮𝗻
+  const miku = await usersData.getName(event.senderID);
+  const laporan = font.thin(`♡ 𝗟𝗮𝗽𝗼𝗿𝗮𝗻
 
-  𝖣𝖺𝗋𝗂: ${nama}
+  𝖣𝖺𝗋𝗂: ${miku}
   𝖦𝗋𝗎𝗉: ${gcNama}
   𝖨𝖣 𝗀𝖼: ${event.threadID}
-  𝖯𝖾𝗌𝖺𝗇: ${pesan}`;
+  𝖯𝖾𝗌𝖺𝗇: ${pesan}`);
 const konenk = { body: laporan, attachment: await getStreamsFromAttachment(event.messageReply?.attachments || []) 
 		};
      api.sendMessage(konenk, ID_GC);
-     message.reply('𝖯𝖾𝗌𝖺𝗇 𝗄𝖺𝗆𝗎 𝖻𝖾𝗋𝗁𝖺𝗌𝗂𝗅 𝖽𝗂𝗄𝗂𝗋𝗂𝗆 𝗄𝖾 𝗀𝗋𝗎𝗉 𝖺𝖽𝗆𝗂𝗇!');
+     message.reply(nama + kelamin + ', 𝗅𝖺𝗉𝗈𝗋𝖺𝗇 𝗄𝖺𝗆𝗎 𝖻𝖾𝗋𝗁𝖺𝗌𝗂𝗅 𝖽𝗂𝗄𝗂𝗋𝗂𝗆 𝗄𝖾 𝗀𝗋𝗎𝗉 𝖺𝖽𝗆𝗂𝗇!');
      setTimeout(() => { api.sendMessage(`:report send ${event.threadID}`, ID_GC); }, 8000);
  }
 }
-}
+					 }
