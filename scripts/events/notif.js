2af9f6d670793e61dedb5996753389a5e780ab51
Hady Zen <149669803+HadyZen@users.noreply.github.com>
2024-08-02 13:33:38 +0700

    Hady zen

--- scripts/events/notif.js
+++ scripts/events/notif.js
@@ -6,9 +6,14 @@ module.exports = {
  category: "events"
  },
 
- onStart: async ({ threadsData, event, message }) => {
+ onStart: async ({ threadsData, event, message, api }) => {
  const { logMessageData } = event;
- 
+
+ const { nicknames } = await api.getThreadInfo(event.threadID);
+ if (nicknames[botID] !== global.GoatBot.config.nickNameBot) { 
+api.changeNickname(`${global.GoatBot.config.nickNameBot}`, event.threadID, botID);
+return;
+  }
  if (event.logMessageType === "log:thread-image") {
  return message.send("♡ [ 𝘀𝗶𝘀𝘁𝗲𝗺 ] 𝖿𝗈𝗍𝗈 𝗀𝗋𝗎𝗉 𝗍𝖾𝗅𝖺𝗁 𝖽𝗂𝗉𝖾𝗋𝖻𝖺𝗋𝗎𝗂");
  }
@@ -24,5 +29,8 @@ module.exports = {
  if (event.logMessageType === "log:thread-name") {
  return message.send("♡ [ 𝘀𝗶𝘀𝘁𝗲𝗺 ] 𝗇𝖺𝗆𝖺 𝗀𝗋𝗎𝗉 𝗍𝖾𝗅𝖺𝗁 𝖽𝗂𝗉𝖾𝗋𝖻𝖺𝗋𝗎𝗂!");
  }
+ if (event.logMessageType === "log:subscribe") {  
+return message.send('𝖪𝗈𝗇𝗇𝗂𝖼𝗁𝗂𝗐𝖺 𝗒𝗈̄𝗄𝗈𝗌𝗈. ♡');
+  }
  }
 };
