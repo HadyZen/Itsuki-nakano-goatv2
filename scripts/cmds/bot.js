36f7d55882b16228fc9ac677c4da6074a6cf424a
Hady Zen <149669803+HadyZen@users.noreply.github.com>
2024-08-02 13:49:12 +0700

    Hady zen

--- scripts/cmds/bot.js
+++ scripts/cmds/bot.js
@@ -1,6 +1,3 @@
-const fs = require('fs');
-const path = require('path');
-
 module.exports = {
   config: {
     name: "bot",
@@ -8,9 +5,9 @@ module.exports = {
     author: "Hady Zen",
     countDown: 10,
     role: 2,
-    category: "SISTEM",
+    category: "ADMIN",
     description: "𝗈𝗎𝗍, 𝗃𝗈𝗂𝗇, 𝖻𝗂𝗇, 𝖽𝖺𝗇 𝗌𝖾𝗇𝖽 𝗉𝖾𝗌𝖺𝗇",
-    guide: { id: "{pn} <𝗈𝗎𝗍/𝗃𝗈𝗂𝗇/𝖻𝗂𝗇> <𝗂𝖽/𝖿𝗂𝗅𝖾>" }
+    guide: { id: "{pn} <𝗈𝗎𝗍/𝗃𝗈𝗂𝗇/𝖼𝗁𝖺𝗍> <𝗂𝖽>" }
   },
   onStart: async function ({ message, args, api, event }) {
     const hadi = args[0];
@@ -18,21 +15,21 @@ module.exports = {
 
     if (hadi === "out") { 
       api.removeUserFromGroup(botID, `${pipi || event.threadID}`);
-message.reaction("✔️", event.messageID);
+message.reaction("❤", event.messageID);
     } else if (hadi === "join") {
       if (!pipi) {
         return message.reply('𝖪𝖺𝗆𝗎 𝗁𝖺𝗋𝗎𝗌 𝗆𝖾𝗆𝖺𝗌𝗎𝗄𝗄𝖺𝗇 𝗂𝖽 𝗀𝗋𝗎𝗉 𝗇𝗒𝖺!');
       }
       return api.addUserToGroup(event.senderID, pipi);
-message.reaction("✔️", event.messageID);
+message.reaction("💙", event.messageID);
     } else if (hadi === "chat") {
       if (!pipi) {
         return message.reply('𝖬𝖺𝗌𝗎𝗄𝗄𝖺𝗇 𝗂𝖽 𝗀𝗋𝗎𝗉 𝗇𝗒𝖺!');
       }
       api.sendMessage('𝖳𝖾𝗋𝗁𝗎𝖻𝗎𝗇𝗀 𝗄𝖾 𝗀𝗋𝗎𝗉!', pipi);
-message.reaction("✔️", event.messageID);
+message.reaction("💚", event.messageID);
     } else {
-      message.reply('𝖾𝗋𝗋𝗈𝗋');
+      message.reply('𝗌𝗎𝖻 𝗉𝖾𝗋𝗂𝗇𝗍𝖺𝗁 𝗍𝗂𝖽𝖺𝗄 𝖺𝖽𝖺 𝗍𝗎𝖺𝗇!');
     }
   }
 };
